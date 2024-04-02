bf63408ec34d190ba916f19d07119530bac25a03
Muhammed Mahmood Alam <muhammed.alam.ext@sivantos.com>
2024-04-02 22:32:41 +0530

    initial Commit

---
+++ docxtohtmlservice/src/main/java/com/fev/docxtohtml/docxtohtmlservice/DocxtohtmlserviceApplication.java
@@ -0,0 +1,13 @@
+package com.fev.docxtohtml.docxtohtmlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocxtohtmlserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocxtohtmlserviceApplication.class, args);
+	}
+
+}
---
+++ docxtohtmlservice/src/test/java/com/fev/docxtohtml/docxtohtmlservice/DocxtohtmlserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fev.docxtohtml.docxtohtmlservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocxtohtmlserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
